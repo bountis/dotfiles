aadfbb99dc0826aa54052f375cc46710f3580ec1
bountis <dominykas.makunas@gmail.com>
2022-07-17 17:39:06 +0300

    fully switched to lua

---
+++ nvim/lua/user/colorscheme.lua
@@ -0,0 +1,53 @@
+vim.o.background = "dark" 
+
+require('lush')(require('apprentice').setup({
+  plugins = {
+    "buftabline",
+    "coc",
+    "cmp", -- nvim-cmp
+    "fzf",
+    "gitgutter",
+    "gitsigns",
+    "lsp",
+    "lspsaga",
+    "nerdtree",
+    "netrw",
+    "nvimtree",
+    "neogit",
+    "packer",
+    "signify",
+    "startify",
+    "syntastic",
+    "telescope",
+    "treesitter"
+  },
+  langs = {
+    "c",
+    "clojure",
+    "coffeescript",
+    "csharp",
+    "css",
+    "elixir",
+    "golang",
+    "haskell",
+    "html",
+    "java",
+    "js",
+    "json",
+    "jsx",
+    "lua",
+    "markdown",
+    "moonscript",
+    "objc",
+    "ocaml",
+    "purescript",
+    "python",
+    "ruby",
+    "rust",
+    "scala",
+    "typescript",
+    "viml",
+    "xml"
+  }
+}))
+
---
+++ nvim/lua/user/plugins.lua
@@ -0,0 +1,71 @@
+local fn = vim.fn
+
+-- Automatically install packer
+local install_path = fn.stdpath "data" .. "/site/pack/packer/start/packer.nvim"
+if fn.empty(fn.glob(install_path)) > 0 then
+  PACKER_BOOTSTRAP = fn.system {
+    "git",
+    "clone",
+    "--depth",
+    "1",
+    "https://github.com/wbthomason/packer.nvim",
+    install_path,
+  }
+  print "Installing packer close and reopen Neovim..."
+  vim.cmd [[packadd packer.nvim]]
+end
+
+-- Autocommand that reloads neovim whenever you save the plugins.lua file
+vim.cmd [[
+  augroup packer_user_config
+    autocmd!
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
+  augroup end
+]]
+
+-- Use a protected call so it doesn't error out on first use
+local status_ok, packer = pcall(require, "packer")
+if not status_ok then
+  return
+end
+
+-- Have packer use a popup window
+packer.init {
+  display = {
+    open_fn = function()
+      return require("packer.util").float { border = "rounded" }
+    end,
+  },
+}
+
+return packer.startup(function(use)
+
+  use "wbthomason/packer.nvim" 
+  use "nvim-lua/popup.nvim" 
+  use "nvim-lua/plenary.nvim" 
+
+  -- colorschemes
+  use {"adisen99/apprentice.nvim", requires = {"rktjmp/lush.nvim"}}
+  use "lunarvim/darkplus.nvim"
+
+  -- cmp
+  use 'hrsh7th/nvim-cmp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use "saadparwaiz1/cmp_luasnip"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/cmp-nvim-lua"
+
+  -- snippets
+  use "L3MON4D3/LuaSnip" 
+  use "rafamadriz/friendly-snippets" 
+
+    -- LSP
+  use "neovim/nvim-lspconfig"
+  use "williamboman/nvim-lsp-installer"
+
+  if PACKER_BOOTSTRAP then
+    require("packer").sync()
+  end
+end)
